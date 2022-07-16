a6eec9cd9153fc10ca3a8eddec6fb5577286d236
M.S <mixa.ru.ru@gmail.com>
2022-07-16 15:00:37 +0300

    Spacy set up and working

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion bitcoins")
+
 for token in doc:
-    print(token.text, token.pos_, token.dep_)
+    print(token.text, token.pos_, token.dep_, token.ent_type_)
