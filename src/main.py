a9123a7603e4f14564c664dbb4cdbfdb3bf35099
M.S <mixa.ru.ru@gmail.com>
2022-07-17 10:56:58 +0300

    refactored 3

--- src/main.py
+++ src/main.py
@@ -1,26 +1,8 @@
 from fastapi import FastAPI
 import spacy
 from typing import List
-from pydantic import BaseModel
 
-
-class Content(BaseModel):
-    post_url: str
-    content: str
-
-
-class Payload(BaseModel):
-    data: List[Content]
-
-
-class SingleEntity(BaseModel):
-    text: str
-    entity_type: str
-
-
-class Entities(BaseModel):
-    post_url: str
-    entities: List[SingleEntity]
+from .models import Payload, Entities
 
 
 app = FastAPI()
